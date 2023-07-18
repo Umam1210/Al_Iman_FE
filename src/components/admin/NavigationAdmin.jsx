d29837ad9e2c32de84cdcaf9d101ca8412db7ff8
Prasetyo Nur Hidayat <prasetyonurhidayat@gmail.com>
2023-07-18 19:58:17 +0700

    init

--- src/components/admin/NavigationAdmin.jsx
+++ src/components/admin/NavigationAdmin.jsx
@@ -1,39 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NavigationAdmin() {
-    const navigate = [
-        { name: 'Katalog', href: 'katalog' },
-        { name: 'Dashboard', href: 'dashboard' },
-        { name: 'Pengguna', href: 'list-pengguna' },
-        { name: 'Produk', href: 'products' },
-        { name: 'Pesanan', href: 'pesanan/menunggu-konfirmasi' },
-        { name: 'Voucher', href: 'voucher' },
-    ]
+  const navigate = [
+    { name: 'Katalog', href: 'katalog' },
+    { name: 'Dashboard', href: 'dashboard' },
+    { name: 'Pengguna', href: 'list-pengguna' },
+    { name: 'Produk', href: 'products' },
+    { name: 'Voucher', href: 'voucher' },
+    { name: 'Pesanan', href: 'pesanan/menunggu-konfirmasi' }
+  ];
 
-    return (
-        <>
-            <div className='w-[344px] h-auto'>
-                <div className='w-full h-[73.66px] bg-[#E9E9E9] flex  items-center '>
-                    <p className='text-[24px] font-normal text-[#000000B2] ml-7'>Navigasi</p>
-                </div>
-                <div className='flex flex-col border-x border-[#E9E9E9]'>
-                    {navigate.map((item, idx) => (
-                        <Link key={idx} to={`/admin/${item.href}`} >
-                            <div className='h-[65.75px] border-b border-[#E9E9E9] flex items-center'>
-                                <p className='text-[24px] font-normal text-[#000000B2] ml-7'>
-                                    {item.name}
-                                </p>
-                            </div>
-                        </Link>
-                    ))}
-                    <div className='h-[65.75px] border-b border-[#E9E9E9] flex items-center'>
-                        <p className='text-[24px] font-normal text-[#000000B2] ml-7'>
-
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="w-[344px] h-auto">
+        <div className="w-full h-[73.66px] bg-[#E9E9E9] flex  items-center ">
+          <p className="text-[24px] font-normal text-[#000000B2] ml-7">Navigasi</p>
+        </div>
+        <div className="flex flex-col border-x border-[#E9E9E9]">
+          {navigate.map((item, idx) => (
+            <Link key={idx} to={`/admin/${item.href}`}>
+              <div className="h-[65.75px] border-b border-[#E9E9E9] flex items-center">
+                <p className="text-[24px] font-normal text-[#000000B2] ml-7">{item.name}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,59 +1,63 @@
-import { Menu, Transition } from '@headlessui/react'
-import React from 'react'
-import { Fragment } from 'react'
-import { useDispatch } from 'react-redux'
-import { logout } from '../../services/login'
-import { useNavigate } from 'react-router-dom'
+import { Menu, Transition } from '@headlessui/react';
+import React from 'react';
+import { Fragment } from 'react';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../services/login';
+import { useNavigate, Link } from 'react-router-dom';
 
 export default function Header({ href }) {
-    const userData = JSON.parse(localStorage.getItem('userData'));
-    const auth = userData?.[3]?.isLogin
-    const guest = userData?.[0]?.role;
-    const dispatch = useDispatch();
-    const navigate = useNavigate()
+  const userData = JSON.parse(localStorage.getItem('userData'));
+  const auth = userData?.[3]?.isLogin;
+  const guest = userData?.[0]?.role;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const handleLogout = () => {
-        dispatch(logout());
-        navigate('/')
-        localStorage.clear()
-    };
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate('/');
+    localStorage.clear();
+  };
 
-    return (
-        <header className="bg-[#000000] ">
-            <nav className="mx-auto h-[85px] flex max-w-full items-center justify-between lg:px-8 md:px-4 sm:px-10 xs:px-6 s:px-4" aria-label="Global">
-                <div className="flex lg:flex-1">
-                    <span className="text-[26px] font-bold text-[#FFFFFF]">Al-Iman Boga</span>
-
-                </div>
-                {auth === undefined ? <div className="flex flex-1 justify-end">
-                    <p className="text-[26px] font-normal text-[#FFFFFF]">
-                        Login
-                    </p>
-                </div> : guest != '' ? <div className="flex flex-1 justify-end">
-                    <Menu as="div" className="relative inline-block text-left">
-                        <div>
-                            <Menu.Button className="text-[26px] font-normal text-[#FFFFFF]">
-                                {href}
-                            </Menu.Button>
-                        </div>
-                        <Transition
-                            as={Fragment}
-                            enter="transition ease-out duration-100"
-                            enterFrom="transform opacity-0 scale-95"
-                            enterTo="transform opacity-100 scale-100"
-                            leave="transition ease-in duration-75"
-                            leaveFrom="transform opacity-100 scale-100"
-                            leaveTo="transform opacity-0 scale-95"
-                        >
-                            <Menu.Items className="absolute right-0 mt-2 h-20 w-32 flex items-center justify-center bg-[#E9E9E9] rounded ring-1 ring-[#000000]">
-                                <button onClick={handleLogout} className='text-[26px] font-semibold'>
-                                    Logout
-                                </button>
-                            </Menu.Items>
-                        </Transition>
-                    </Menu>
-                </div> : ''}
-            </nav>
-        </header>
-    )
+  return (
+    <header className="bg-[#000000] ">
+      <nav
+        className="mx-auto h-[85px] flex max-w-full items-center justify-between lg:px-8 md:px-4 sm:px-10 xs:px-6 s:px-4"
+        aria-label="Global"
+      >
+        <Link to={'/'}>
+          <div className="flex lg:flex-1">
+            <span className="text-[26px] font-bold text-[#FFFFFF]">Al-Iman Boga</span>
+          </div>{' '}
+        </Link>
+        {auth === undefined ? (
+          <div className="flex flex-1 justify-end"></div>
+        ) : guest != '' ? (
+          <div className="flex flex-1 justify-end">
+            <Menu as="div" className="relative inline-block text-left">
+              <div>
+                <Menu.Button className="text-[26px] font-normal text-[#FFFFFF]">{href}</Menu.Button>
+              </div>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 mt-2 h-15 w-28 flex items-center justify-center text-white bg-[#dc6969] rounded ">
+                  <button onClick={handleLogout} className="text-[26px] font-semibold">
+                    Logout
+                  </button>
+                </Menu.Items>
+              </Transition>
+            </Menu>
+          </div>
+        ) : (
+          ''
+        )}
+      </nav>
+    </header>
+  );
 }
